b92a8f7696d54f54a81d98617946a2030e16e232
Jonah Grossberg <jonahag@gmail.com>
2022-05-22 16:13:13 -0400

    add models and admin

--- betting/admin.py
+++ betting/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Bet, Bettor, Game
+from .models import Bet, Bettor, Game, Line, Team
 
 
 class BetAdmin(admin.ModelAdmin):
-    fields = ['bettor', 'game', 'amount']
+    fields = ['bettor', 'game', 'line', 'odds', 'amount']
     
 class BettorAdmin(admin.ModelAdmin):
     fields = ['user', 'balance']
@@ -12,6 +12,14 @@ class BettorAdmin(admin.ModelAdmin):
 class GameAdmin(admin.ModelAdmin):
     fields = ['team1', 'team2', 'winner']
     
+class LineAdmin(admin.ModelAdmin):
+    fields = ['type', 'game', 'team', 'points', 'odds', 'side']
+
+class TeamAdmin(admin.ModelAdmin):
+    fields = ['city', 'name', 'sport']
+
 admin.site.register(Bet, BetAdmin)
 admin.site.register(Bettor, BettorAdmin)
 admin.site.register(Game, GameAdmin)
+admin.site.register(Line, LineAdmin)
+admin.site.register(Team, TeamAdmin)
--- betting/models.py
+++ betting/models.py
@@ -2,17 +2,54 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Bettor(models.Model):
-    balance = models.DecimalField(decimal_places=4, max_digits=32)
     user = models.ForeignKey(User, on_delete = models.CASCADE, blank = False, null = False)
+    balance = models.DecimalField(decimal_places=4, max_digits=32)
+
+    def __str__(self):
+        return str.format("{}:{}", self.user, self.balance)
+
+class Team(models.Model):
+    city = models.TextField()
+    name = models.TextField()
+    sport = models.TextField()
+
+    def __str__(self):
+        return str.format("{} {}", self.city, self.name)
+
 
 class Game(models.Model):
-    team1 = models.TextField()
-    team2 = models.TextField()
+    team1 = models.ForeignKey(Team, related_name="team_1", on_delete=models.DO_NOTHING, blank=False, null=False)
+    team2 = models.ForeignKey(Team, related_name="team_2", on_delete=models.DO_NOTHING, blank=False, null=False)
     status = models.TextField()
     winner = models.TextField()
+
+    def __str__(self):
+        return str.format("{} vs {}", self.team1, self.team2)
+
+
+class Line(models.Model):
+    OVER_UNDER_CHOICES = [("OVER", "over"), ("UNDER", "under")]
+    TYPE_CHOICES = [("TOTAL", "Total"), ("ML", "Moneyline"), ("SPREAD", "Spread")]
+
+    type = models.CharField(max_length=15, choices=TYPE_CHOICES)
+    game = models.ForeignKey(Game, on_delete=models.PROTECT, blank=False, null=False)
+    team = models.ForeignKey(Team, on_delete=models.DO_NOTHING, blank=True, null=True)
+    points = models.DecimalField(decimal_places=2, max_digits=4)
+    odds = models.DecimalField(decimal_places=0, max_digits=5)
+    side = models.CharField(max_length=10, choices=OVER_UNDER_CHOICES, blank=True)
+
+    def __str__(self):
+        return str.format("{} {}{} {}", self.type, self.side, self.points, self.odds)
+
 
 class Bet(models.Model):
     bettor = models.ForeignKey(Bettor, on_delete = models.DO_NOTHING, blank = False, null = False)
     game = models.ForeignKey(Game, on_delete=models.PROTECT, blank=False, null=False)
+    line = models.ForeignKey(Line, on_delete=models.PROTECT, blank=False, null=False)
     amount = models.DecimalField(decimal_places=4, max_digits=16)
+    odds = models.IntegerField()
+
+    def __str__(self):
+        return str.format("{} - {}: {}", self.bettor, self.amount, self.game)
+
 
